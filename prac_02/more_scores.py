4bb558d995678a03f2fce0926ec34d3905bf6fef
miapar <mdparnell13@gmail.com>
2023-09-24 15:22:58 +1000

    Add a main function to the program

--- prac_02/more_scores.py
+++ prac_02/more_scores.py
@@ -1,11 +1,30 @@
+"""
+Program to get a number of scores, generate random scores, and print the category the score is in.
+"""
+
+
+import random
+
+
+def main():
+    """Generate random scores and print their category."""
+    number_of_scores = int(input("Number of scores: "))
+    for score in range(number_of_scores):
+        score = random.randint(0, 100)
+        print(f"{score} is {determine_score_category(score)}")
+
+
 def determine_score_category(score):
     """Determine what category a score is in."""
     if score < 0 or score > 100:
         return "Invalid score"
     else:
         if score < 50:
-            return "Your score is bad"
+            return "bad"
         elif score < 90:
-            return "You passed"
+            return "passable"
         else:
-            return "You did excellent!"
+            return "excellent"
+
+
+main()
