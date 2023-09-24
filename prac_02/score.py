c4febca8b6b3790efb95400cb13ab99d80fd5f19
miapar <mdparnell13@gmail.com>
2023-09-24 14:48:42 +1000

    Add functions and a random score generator

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,29 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score < 50:
-        print("Your score is bad")
-    elif score < 90:
-        print("You passed")
+import random
+
+
+def main():
+    """Get a score and print its category."""
+    score = float(input("Enter score: "))
+    print(determine_score_category(score))
+    score = random.randint(0, 100)
+    print(f"Random score: {score}")
+    print(determine_score_category(score))
+
+
+def determine_score_category(score):
+    """Determine what category a score is in."""
+    if score < 0 or score > 100:
+        return "Invalid score"
     else:
-        print("You did excellent!")
+        if score < 50:
+            return "Your score is bad"
+        elif score < 90:
+            return "You passed"
+        else:
+            return "You did excellent!"
+
+
+main()
