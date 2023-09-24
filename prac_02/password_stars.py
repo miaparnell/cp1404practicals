d938d91fb2f6636d453ed5b1dd6723498958f79c
miapar <mdparnell13@gmail.com>
2023-09-24 14:27:19 +1000

    Add docstrings and good formatting

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,13 +1,20 @@
+"""
+Program to get the user to enter a valid password and print stars according to the length
+"""
+
+
 MINIMUM_LENGTH = 8
 
 
 def main():
+    """Get a password and print a sequence of stars"""
     password = input("Password: ")
     password = get_password(password)
     print_stars(password)
 
 
 def get_password(password):
+    """Determine if a password is valid."""
     while len(password) < MINIMUM_LENGTH:
         print("Password too short")
         password = input("Password: ")
@@ -15,6 +22,7 @@ def get_password(password):
 
 
 def print_stars(password):
+    """Print stars for the length of a password."""
     for i in range(len(password)):
         print("*", end='')
 
