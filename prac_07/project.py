ad9e239d5d86776cc970c1d95fcfed20e2603158
miapar <mdparnell13@gmail.com>
2023-11-05 23:59:06 +1000

    Update projects

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,6 +1,23 @@
 class Project:
-    def __init__(self, name="", start_date=0, cost_estimate=0, completion_percentage=0):
+    def __init__(self, name="", start_date="", priority=0, cost_estimate=0, completion_percentage=0):
         self.name = name
         self.start_date = start_date
+        self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Format information about projects."""
+        return f"{self.name}, start:{self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%"
+
+    def is_complete(self):
+        """Determine if a project is complete."""
+        return self.completion_percentage == 100
+
+    def is_not_complete(self):
+        """Determine if a project is complete."""
+        return self.completion_percentage != 100
+
+    def __gt__(self, other):
+        """Determine if a one data is past another date."""
+        return self.start_date > other.start_date
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,16 +1,19 @@
 """
 Time estimate: 2 hours
-Actual time:
+Actual time: 2 hours 15 minutes
 """
 
 
 from project import Project
+import datetime
 
-MENU = "(L)oad\n(S)ave\n(D)isplay\n(F)ilter\n(A)\n(U)pdate"
+MENU = "(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by date\n(A)dd new project\n(U)pdate project\n(Q)uit"
+
 
 def main():
     in_filename = 'projects.txt'
     projects = load_data(in_filename)
+    print(MENU)
     choice = input("Choice: ").upper()
     while choice != "Q":
         if choice == "L":
@@ -20,13 +23,13 @@ def main():
             out_filename = input("Filename: ")
             save_projects(out_filename, projects)
         elif choice == "D":
-
+            display_projects(projects)
         elif choice == "F":
-
+            filter_projects(projects)
         elif choice == "A":
-
+            add_project(projects)
         elif choice == "U":
-
+            update_project(projects)
         else:
             print("Invalid choice")
         choice = input("Choice: ").upper()
@@ -40,16 +43,56 @@ def load_data(in_filename):
     in_file = open(in_filename, 'r')
     in_file.readline()
     for line in in_file:
-        parts = line.strip().split('    ')
-        project = Project(parts[0], parts[1], float(parts[2]), int(parts[4]))
+        parts = line.strip().split('\t')
+        project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
         projects.append(project)
     in_file.close()
     return projects
+
+
+def display_projects(projects):
+    for i, project in enumerate(projects, 1):
+        print("Completed projects: ")
+        if project.is_complete():
+            print(project)
+        print("Incomplete projects: ")
+        if project.is_not_complete():
+            print(project)
+
+
+def filter_projects(projects):
+    date_string = input("Date (d/m/yyyy): ")
+    date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+
+
+def add_project(projects):
+    print("Let's add a new project!")
+    name = input("Name: ")
+    start_date = input("Start date (d/m/yyyy):")
+    priority = int(input("Priority: "))
+    cost_estimate = float(input("Cost estimate: $"))
+    completion_percentage = int(input("Completion percentage: "))
+    new_project = Project(name, start_date, priority, cost_estimate, completion_percentage)
+    projects.append(new_project)
+
+
+def update_project(projects):
+    project_number = 0
+    for project in projects:
+        print(project_number, project)
+        project_number += 1
+    project_choice = int(input("Project choice: "))
+    print(projects[project_choice])
+    new_percentage = int(input("New percentage: "))
+    projects[project_choice][4] = new_percentage
+    new_priority = int(input("New priority: "))
+    projects[project_choice][2] = new_priority
+
 
 def save_projects(out_filename, projects):
     with open(out_filename, 'w', newline="") as out_file:
         for project in projects:
             out_file.write('    '.join(project) + '\n')
 
-def display_projects():
 
+main()
